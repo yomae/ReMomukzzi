05d825b3c128e9e50c4a6bf39ae57016b227cb9b
yomae <goldrunkim@naver.com>
2023-01-17 00:46:22 +0900

    server api change

--- client/pages/index.js
+++ client/pages/index.js
@@ -118,7 +118,7 @@ const Home = () => {
                     console.log(res.length);
                     axios
                       .post(
-                        "http://ec2-54-180-143-191.ap-northeast-2.compute.amazonaws.com:8080/data",
+                        "https://noqz6gs8ia.execute-api.ap-northeast-2.amazonaws.com/data",
                         { data: res },
                         {
                           withCredentials: true,
